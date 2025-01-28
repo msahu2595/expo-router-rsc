534d298526d32f5c5b20d7de8b46a8ede71c4aa9
msahu2595 <msahu2595@gmail.com>
2025-01-28 19:16:25 +0530

    [ADD] Image uploading using expo server functions.

---
+++ actions/images.tsx
@@ -0,0 +1,59 @@
+"use server";
+import fs from "node:fs";
+import path from "node:path";
+import { buffer } from "node:stream/consumers";
+
+export interface StoredImage {
+    fileName: string;
+    originalFileName: string;
+    width: number;
+    height: number;
+}
+
+const IMAGES_JSON_PATH = path.join(".", "images.json");
+
+export async function postImage({
+    name: originalFileName,
+    image,
+    width,
+    height,
+}: {
+    name: string;
+    image: string;
+    width: number;
+    height: number;
+}) {
+    try {
+        const buffer = Buffer.from(image, "base64");
+
+        const dir = path.join(process.cwd(), "public", "images");
+        if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir, { recursive: true })
+        }
+
+        const randomString = Math.random().toString(36).substring(2, 15);
+        const fileExtension = originalFileName.split('.').pop() || "jpg";
+        const name = `${randomString}.${fileExtension}`;
+
+        const filePath = path.join(dir, name);
+        fs.writeFileSync(filePath, buffer);
+
+        const images: StoredImage[] = [];
+
+        images.push({
+            fileName: name,
+            originalFileName,
+            width,
+            height
+        })
+
+        fs.writeFileSync(IMAGES_JSON_PATH, JSON.stringify(images, null, 2));
+
+        return { success: true }
+    } catch (error) {
+        console.error("Error saving image: ", error)
+        return { success: false, error: "Failed to save image" }
+    }
+
+
+};
